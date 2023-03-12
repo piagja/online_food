cac2a9b7ada74463147447eea37f6e207908488b
piagja <piagja@gmail.com>
2023-03-12 03:36:30 -0300

    feat: add root path

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
@@ -6,8 +6,16 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'ok'
+  })
+})
+
 export const start = (PORT: number) => {
   app.listen(PORT, () => {
     console.info(`Server running in ${PORT} port`)
   })
 }
+
+export { app }

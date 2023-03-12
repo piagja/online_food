5613dd6e51c80610e0e9ba7a1b517e7ef6a16fa7
piagja <piagja@gmail.com>
2023-03-12 03:36:48 -0300

    test: server testing

--- src/tests/server/server.test.ts
+++ src/tests/server/server.test.ts
@@ -1,3 +1,29 @@
-test('dois mais dois igual a 4', () => {
-  expect(2 + 2).toBe(4)
+require('dotenv').config()
+
+import { app } from '../../App'
+
+import * as http from 'http'
+import supertest from 'supertest'
+
+const { API_PORT } = process.env
+
+let server: http.Server
+let request: supertest.SuperTest<supertest.Test>
+
+beforeAll(clear => {
+  server = app.listen(API_PORT, () => {
+    request = supertest(server)
+    clear()
+  })
+})
+
+afterAll(clear => {
+  server.close(clear())
+})
+
+describe('Server Layer', () => {
+  it('Should listening at dotenv API PORT', async () => {
+    const response = await request.get('/')
+    expect(response.status).toBe(200)
+  })
 })
